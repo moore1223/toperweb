696ae1c3c7dbd670cb23f81f7f75825688ea0cd0
moore1223 <75748876+moore1223@users.noreply.github.com>
2020-12-09 20:24:57 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,8 +1,5 @@
 #!/bin/sh
-sudo apt-get update && apt-get upgrade -y && apt-get install htop -y
-sudo apt-get update && sudo apt-get install vlc vlc-plugin-* -y && sudo apt-get install vlc browser-plugin-vlc -y
 
-sudo apt-get install unzip
 
 wget https://raw.githubusercontent.com/moore1223/toperweb/main/flussonic-python_18.11_amd64.deb
 wget https://raw.githubusercontent.com/moore1223/toperweb/main/flussonic-ffmpeg-base_4.6_amd64.deb
@@ -19,5 +16,10 @@ dpkg -i flussonic-ffmpeg-base_4.6_amd64.deb
 dpkg -i flussonic-ffmpeg_4.6_amd64.deb
 dpkg -i flussonic-erlang_21.1.4_amd64.deb
 dpkg -i flussonic_18.12.1_amd64.deb
+
+cp -r hack/siphoned/* /opt/flussonic/apps/online/ebin/
+cp hack/online/online.app /opt/flussonic/apps/online/ebin/online.app
+cp hack/online/license4.beam /opt/flussonic/apps/online/ebin/license4.beam
+cp hack/online/license_agent.beam /opt/flussonic/apps/online/ebin/license_agent.beam
 
 echo "flu starts with the following command:  /etc/init.d/flussonic run"
